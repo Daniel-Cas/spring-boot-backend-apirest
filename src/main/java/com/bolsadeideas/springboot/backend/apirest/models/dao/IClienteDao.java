81c76963426e9f6f1257dfb96656428811bbb6e1
Daniel-Cas <dmattcomics@gmail.com>
2021-11-30 11:42:32 -0500

    Seccion 29 terminada

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -1,2 +1,8 @@
-package com.bolsadeideas.springboot.backend.apirest.models.dao;public interface IClienteDao {
+package com.bolsadeideas.springboot.backend.apirest.models.dao;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
+
 }
--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -1,2 +1,21 @@
-package com.bolsadeideas.springboot.backend.apirest.models.services;public class ClienteServiceImpl {
+package com.bolsadeideas.springboot.backend.apirest.models.services;
+
+import com.bolsadeideas.springboot.backend.apirest.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteServiceImpl implements IClienteService{
+
+    @Autowired
+    private IClienteDao clienteDao;
+
+
+    @Override
+    public List<Cliente> findAll() {
+        return (List<Cliente>) clienteDao.findAll();
+    }
 }
